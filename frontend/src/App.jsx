ea8156ce9427f66800cbf7d231099cfe80b95011
sadafshahab12 <sadafshahab123@yahoo.com>
2025-07-23 10:17:40 +0500

    setup router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 
 function App() {
- 
-
   return (
- <>
- 
- </>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
